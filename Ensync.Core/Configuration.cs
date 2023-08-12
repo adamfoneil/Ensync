fc28135ad2bdfceba3ec710688e1b6945d01e77f
Adam O'Neil <adamosoftware@gmail.com>
2023-08-12 19:12:10 -0400

    cleanup

--- Ensync.Core/Configuration.cs
+++ Ensync.Core/Configuration.cs
@@ -3,15 +3,15 @@
 
 public class Configuration
 {
-    public required string AssemblyPath { get; set; }
-    public Target[] DatabaseTargets { get; set; } = Array.Empty<Target>();
+	public required string AssemblyPath { get; set; }
+	public Target[] DatabaseTargets { get; set; } = Array.Empty<Target>();
 	public ScriptAction[] Ignore { get; set; } = Array.Empty<ScriptAction>();
 
 	public class Target
-    {
-        public required string Name { get; init; }
-        public required string Type { get; init; }
-        public required string ConnectionString { get; init; }
-        public bool IsProduction { get; init; } // if true, then CLI prompts you to type the database name
-    }    
+	{
+		public required string Name { get; init; }
+		public required string Type { get; init; }
+		public required string ConnectionString { get; init; }
+		public bool IsProduction { get; init; } // if true, then CLI prompts you to type the database name
+	}    
 }
