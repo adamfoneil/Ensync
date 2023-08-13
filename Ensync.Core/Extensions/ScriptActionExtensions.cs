e96a03f8c6ea8c954903a9d3698aab8951c37e22
Adam O'Neil <adamosoftware@gmail.com>
2023-08-13 17:17:01 -0400

    ensure unique commands

--- Ensync.Core/Extensions/ScriptActionExtensions.cs
+++ Ensync.Core/Extensions/ScriptActionExtensions.cs
@@ -5,7 +5,7 @@ namespace Ensync.Core.Extensions;
 public static class ScriptActionExtensions
 {
 	public static IEnumerable<string> ToSqlStatements(this IEnumerable<ScriptAction> actions, SqlScriptBuilder scriptBuilder, bool allowDestruction = false) =>
-		actions.SelectMany(a => a.Statements.Select(cmd => a.IsDestructive && !allowDestruction ? scriptBuilder.ToBlockComment(cmd.Sql) : cmd.Sql));
+		actions.SelectMany(a => a.Statements.Select(cmd => a.IsDestructive && !allowDestruction ? scriptBuilder.ToBlockComment(cmd.Sql) : cmd.Sql)).ToHashSet();
 
 	public static string ToSqlScript(this IEnumerable<ScriptAction> actions, string separator, SqlScriptBuilder scriptBuilder, bool allowDestruction = false) =>
 		string.Join(separator, ToSqlStatements(actions, scriptBuilder, allowDestruction));
