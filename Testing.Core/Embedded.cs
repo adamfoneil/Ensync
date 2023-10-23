0da8b80a35178e7e6956da84b41d9e15b210e3dc
Adam O'Neil <adamosoftware@gmail.com>
2023-10-23 12:06:52 -0400

    fixed a test

--- Testing.Core/Embedded.cs
+++ Testing.Core/Embedded.cs
@@ -15,8 +15,9 @@ public class Embedded
 	public async Task UnexpectedUniqueDrop() => await TestEmbeddedAsync("Testing.Core.EmbeddedCases.UnexpectedUniqueDrop.zip", new[]
 	{
 		"ALTER TABLE [dbo].[WidgetType] ALTER COLUMN [Name] nvarchar(50) NOT NULL",
-		"ALTER TABLE [dbo].[WidgetType] ADD CONSTRAINT [U_WidgetType_Name] UNIQUE ([Name] ASC)"
-	});
+		"ALTER TABLE [dbo].[WidgetType] ADD CONSTRAINT [U_WidgetType_Name] UNIQUE ([Name] ASC)",
+        "ALTER TABLE [dbo].[Widget] ADD CONSTRAINT [FK_Widget_TypeId] FOREIGN KEY ([TypeId]) REFERENCES [dbo].[WidgetType] ([Id])"
+    });
 
 	[TestMethod]
 	public async Task ShouldRebuildFK() => await TestEmbeddedAsync("Testing.Core.EmbeddedCases.ShouldRebuildFK.zip", new[]
